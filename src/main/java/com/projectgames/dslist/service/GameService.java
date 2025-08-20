694139b0eb2f78570ff9d258cd4efc7d5c056d2b
ccaiosantos <caiomedeiros1010@gmail.com>
2025-08-20 19:48:43 -0300

    buscando game por id

--- src/main/java/com/projectgames/dslist/service/GameService.java
+++ src/main/java/com/projectgames/dslist/service/GameService.java
@@ -1,10 +1,13 @@
 package com.projectgames.dslist.service;
 
+import com.projectgames.dslist.controller.GameController;
+import com.projectgames.dslist.dto.GameDTO;
 import com.projectgames.dslist.dto.GameMinDTO;
 import com.projectgames.dslist.entities.Games;
 import com.projectgames.dslist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +17,12 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findByid(Long id){
+        Games result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findall(){
         List<Games> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList(); //o to list vai servir para retornar uma list //a lista de games vai retornar a lista de gamesmindto
